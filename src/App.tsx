b50d578cddc9a8109b56fdca807ff9dba2b4f2c6
1devndogs <leo@subspace.network>
2021-10-08 23:38:37 -0300

    Clean sccs, build new css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import { WsProvider } from "@polkadot/rpc-provider";
-import { HealthContextProvider, SystemContextProvider } from "context";
-import { ApiPromiseContextProvider } from "context";
+import {
+  HealthContextProvider,
+  SystemContextProvider,
+  RelayerContextProvider,
+  ApiPromiseContextProvider,
+} from "context";
 import MainLayout from "layout/MainLayout";
 
-const WS_PROVIDER = process.env.REACT_APP_WS_PROVIDER || "ws://localhost:9944";
+const WS_PROVIDER = process.env.REACT_APP_WS_PROVIDER;
 
 const App = () => {
   if (!WS_PROVIDER) {
     console.error("WS_PROVIDER not found from env, connecting to a local node");
   }
 
-  const [provider] = useState<WsProvider>(new WsProvider(WS_PROVIDER));
+  const [provider] = useState<WsProvider>(
+    new WsProvider(WS_PROVIDER || "ws://localhost:9944")
+  );
 
   return (
     <SystemContextProvider provider={provider}>
       <HealthContextProvider provider={provider}>
         <ApiPromiseContextProvider provider={provider}>
-          <MainLayout />
+          <RelayerContextProvider provider={provider}>
+            <MainLayout />
+          </RelayerContextProvider>
         </ApiPromiseContextProvider>
       </HealthContextProvider>
     </SystemContextProvider>
