448890a7b2269a0a40f0cb0943e964f0d100f99f
1devndogs <leo@subspace.network>
2021-10-09 21:22:32 -0300

    Fix undefined label on 0 Bytes

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,4 +1,5 @@
 export const bytesToSize = (bytes: number): string => {
+  if(bytes === 0) return '0 B';
   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
   const i = parseFloat(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
   if (i === 0) return `${bytes} ${sizes[i]}`;
