4d8049773161073b9b3c9f5adb5d30dee114f56c
1devNdogs <leo@subspace.network>
2021-10-13 10:39:13 -0300

    Update src/components/utils.ts
    Fix size labels
    
    Co-authored-by: Nazar Mokrynskyi <nazar@mokrynskyi.com>

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,6 +1,6 @@
 export const bytesToSize = (bytes: number): string => {
   if(bytes === 0) return '0 B';
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  const sizes = ["Bytes", "KiB", "MiB", "GiB", "TiB"];
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
   if (i === 0) return `${bytes} ${sizes[i]}`;
   return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
