cc68288668c8c252bf81a690b3c345338124ef1f
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-10-20 18:24:08 +0300

    Store downloaded blocks in RocksDB

--- backend/src/tools/download-substrate-blocks.ts
+++ backend/src/tools/download-substrate-blocks.ts
@@ -4,9 +4,13 @@
 import {ApiPromise, WsProvider} from "@polkadot/api";
 import {firstValueFrom} from "rxjs";
 import * as fs from "fs/promises";
+// TODO: Types do not seem to match the code, hence usage of it like this
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const levelup = require("levelup");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const rocksdb = require("rocksdb");
 
-const REPORT_PROGRESS_INTERVAL = process.env.REPORT_PROGRESS_INTERVAL ? parseInt(process.env.REPORT_PROGRESS_INTERVAL) : 100;
-const BLOCK_NUMBER_CHUNK_REGEX = /.{1,2}/g;
+const REPORT_PROGRESS_INTERVAL = process.env.REPORT_PROGRESS_INTERVAL ? BigInt(process.env.REPORT_PROGRESS_INTERVAL) : 100n;
 
 (async () => {
   const source_chain_rpc = process.env.SOURCE_CHAIN_RPC;
@@ -44,35 +48,32 @@ const BLOCK_NUMBER_CHUNK_REGEX = /.{1,2}/g;
 
   console.log(`Downloading blocks into ${target_dir}`);
 
+  const db = levelup(rocksdb(`${target_dir}/db`));
+
   const lastDownloadedBlock = await (async () => {
     try {
-      return parseInt(await fs.readFile(`${target_dir}/last-downloaded-block`, {encoding: 'utf-8'}));
+      return BigInt(await fs.readFile(`${target_dir}/last-downloaded-block`, {encoding: 'utf-8'}));
     } catch {
-      return -1;
+      return -1n;
     }
   })();
 
   if (lastDownloadedBlock > -1) {
-    console.info(`Continuing downloading from block ${lastDownloadedBlock + 1}`);
+    console.info(`Continuing downloading from block ${lastDownloadedBlock + 1n}`);
   }
 
   let lastDownloadingReportAt;
 
-  for (let blockNumber = lastDownloadedBlock + 1; blockNumber <= lastFinalizedBlockNumber; ++blockNumber) {
+  for (let blockNumber = lastDownloadedBlock + 1n; blockNumber <= lastFinalizedBlockNumber; ++blockNumber) {
     const blockHash = await firstValueFrom(api.rx.rpc.chain.getBlockHash(blockNumber));
     const blockBytes = (await firstValueFrom(api.rx.rpc.chain.getBlock(blockHash))).toU8a();
 
-    // We know it is a number and always matches, hence suppression
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const blockDir = blockNumber.toString().match(BLOCK_NUMBER_CHUNK_REGEX)!.join('/');
+    await db.put(Buffer.from(BigUint64Array.of(blockNumber).buffer), Buffer.from(blockBytes));
 
-    await fs.mkdir(`${target_dir}/${blockDir}`, {recursive: true});
-    await fs.writeFile(`${target_dir}/${blockDir}/block-${blockNumber}`, blockBytes)
-
-    if (blockNumber % REPORT_PROGRESS_INTERVAL === 0) {
+    if (blockNumber % REPORT_PROGRESS_INTERVAL === 0n) {
       const now = Date.now();
       const downloadRate = lastDownloadingReportAt
-        ? ` (${(REPORT_PROGRESS_INTERVAL / ((now - lastDownloadingReportAt) / 1000)).toFixed(2)} blocks/s)`
+        ? ` (${(Number(REPORT_PROGRESS_INTERVAL) / ((now - lastDownloadingReportAt) / 1000)).toFixed(2)} blocks/s)`
         : "";
       lastDownloadingReportAt = now;
 
