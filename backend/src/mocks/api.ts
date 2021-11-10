240feb0b70d262ff6c0cda5271c10cf6f8c74775
serge <isserge@protonmail.com>
2021-11-10 17:02:30 +0400

    added logger mock and helper function to mock polkadot.js API

---
+++ backend/src/mocks/api.ts
@@ -0,0 +1,17 @@
+import { ApiPromise } from "@polkadot/api";
+import { SignerOptions } from "@polkadot/api/types";
+import { Callback, ISubmittableResult } from '@polkadot/types/types';
+
+export const createMockPutWithResult = (result: ISubmittableResult): ApiPromise => ({
+  tx: {
+    feeds: {
+      put() {
+        return {
+          signAndSend(_account: string, _options: SignerOptions, statusCallback: Callback<ISubmittableResult>) {
+            statusCallback(result)
+          }
+        }
+      }
+    }
+  }
+}) as unknown as ApiPromise;
