1fd47e81ad69d85c29b8855372850913c615fa48
serge <isserge@protonmail.com>
2021-11-15 13:49:50 +0200

    added tests for sendBlocksBatchTx

--- backend/src/test/target.test.ts
+++ backend/src/test/target.test.ts
@@ -18,19 +18,28 @@ tap.test('Target module', (t) => {
     metadata: Buffer.from([]),
   }
   const nonce = BigInt(0);
+  const txBatch = [txBlock, txBlock];
+
+  const putSuccessResult = {
+    isError: false,
+    status: {
+      isInBlock: true,
+      asInBlock: 'random hash'
+    }
+  } as unknown as ISubmittableResult;
+
+  const putFailureResult = {
+    isError: true,
+    status: {
+      isInvalid: true
+    }
+  } as unknown as ISubmittableResult;
+
+  const apiSuccess = createMockPutWithResult(putSuccessResult);
+  const apiFailure = createMockPutWithResult(putFailureResult);
 
   tap.test('sendBlockTx should successfully send block transaction and resolve hash', async (t) => {
-    const putSuccessResult = {
-      isError: false,
-      status: {
-        isInBlock: true,
-        asInBlock: 'random hash'
-      }
-    } as unknown as ISubmittableResult;
-
-    const api = createMockPutWithResult(putSuccessResult);
-
-    const target = new Target({ api, logger, targetChainUrl });
+    const target = new Target({ api: apiSuccess, logger, targetChainUrl });
 
     const hash = await target.sendBlockTx(feedId, chainName, signer, txBlock, nonce);
 
@@ -38,23 +47,24 @@ tap.test('Target module', (t) => {
   });
 
   tap.test('sendBlockTx should reject if polkadot.js API throws error', async (t) => {
-    const putFailureResult = {
-      isError: true,
-      status: {
-        isInvalid: true
-      }
-    } as unknown as ISubmittableResult;
-
-    const api = createMockPutWithResult(putFailureResult);
-
-    const target = new Target({ api, logger, targetChainUrl });
+    const target = new Target({ api: apiFailure, logger, targetChainUrl });
 
     t.rejects(target.sendBlockTx(feedId, chainName, signer, txBlock, nonce));
   });
 
-  tap.test('sendBlocksBatchTx should successfully send batch of transactions and resolve hash');
+  tap.test('sendBlocksBatchTx should successfully send batch of transactions and resolve hash', async (t) => {
+    const target = new Target({ api: apiSuccess, logger, targetChainUrl });
 
-  tap.test('sendBlocksBatchTx should reject if polkadot.js API throws error');
+    const hash = await target.sendBlocksBatchTx(feedId, chainName, signer, txBatch, nonce);
+
+    t.equal(hash, putSuccessResult.status.asInBlock);
+  });
+
+  tap.test('sendBlocksBatchTx should reject if polkadot.js API throws error', async (t) => {
+    const target = new Target({ api: apiFailure, logger, targetChainUrl });
+
+    t.rejects(target.sendBlocksBatchTx(feedId, chainName, signer, txBatch, nonce));
+  });
 
   t.end();
 })
