2177195c9ebb78daeddecb72a9375d772765905a
serge <isserge@protonmail.com>
2021-11-30 11:01:31 +0200

    create target api for each chain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,17 +47,17 @@ function createApi(url: string): Promise<ApiPromise> {
 }
 
 async function main() {
-  const targetApi = await createApi(config.targetChainUrl);
-
-  const target = new Target({
-    api: targetApi,
-    logger,
-    targetChainUrl: config.targetChainUrl,
-  });
   const chainHeadStateMap = new Map<ChainId, PrimaryChainHeadState | ParachainHeadState>();
-
   const processingChains = [config.primaryChain, ...config.parachains]
     .map(async (chainConfig: PrimaryChainConfig | ParachainConfig) => {
+      const targetApi = await createApi(config.targetChainUrl);
+
+      const target = new Target({
+        api: targetApi,
+        logger,
+        targetChainUrl: config.targetChainUrl,
+      });
+
       const chainName = await pRetry(
         () => getChainName(chainConfig.httpUrl),
         {
